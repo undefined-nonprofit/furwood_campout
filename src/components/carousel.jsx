34db4f51483408831f1eb2909692a329da33a34b
Robert Von Zimmerman <vonzimr@gmail.com>
2025-02-18 22:24:06 -0800

    fix messed up carousel

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -2,7 +2,7 @@ import { Carousel } from "flowbite-react";
 
 export default ({ children }) => {
     return (
-        <div className="h-56 sm:h-64 xl:h-80 2xl:h-96 w-full">
+        <div className="h-56 sm:h-64 xl:h-80 2xl:h-96 w-fit">
             <Carousel leftControl={<></>} rightControl={<></>}>
                 {children}
             </Carousel>
